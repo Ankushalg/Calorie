8fdc3445e0bbac2769d0505a057ccfd7dad8b761
Ankush Kumar <ankushalg@gmail.com>
2020-05-30 17:27:11 +0530

    Updated Launch Activity Methods

--- app/src/main/java/com/allstudio/calorie/LaunchActivity.kt
+++ app/src/main/java/com/allstudio/calorie/LaunchActivity.kt
@@ -1,12 +1,21 @@
 package com.allstudio.calorie
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 
 class LaunchActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_launch)
+        supportActionBar?.hide()
+        object: CountDownTimer(2000, 2000){
+            override fun onTick(milli: Long){}
+            override fun onFinish(){
+                val i = Intent(this@LaunchActivity, MainActivity::class.java)
+                startActivity(i)
+            }
+        }.start()
     }
 }
