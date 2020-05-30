af685a27f4d4c6be061bcf85c85dfb785e22280e
Ankush Kumar <ankushalg@gmail.com>
2020-05-30 17:15:17 +0530

    Added Launch Activity and Updated Launch Activity UI

---
+++ app/src/main/java/com/allstudio/calorie/LaunchActivity.kt
@@ -0,0 +1,12 @@
+package com.allstudio.calorie
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LaunchActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_launch)
+    }
+}
